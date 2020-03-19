90c9706156fb4be17ef83cafc8037de7d313a194
Calo Corp <marc.calo@codeimmersives.com>
2020-03-19 12:01:41 -0400

    eh

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ const acquireColor = function (el){
     return el.classlist[1];
 }
 
+ const grabBrush= function (event) {
+    document.querySelector('.currentbrush')
+}
+
+const colorBrush = function (event) {
+
+    grabBrush.re
+
+}
+
+.addEventListener('click',)
